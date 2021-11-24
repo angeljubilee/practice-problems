c8c0aaffcda920addbde89f7f2bf7a36daa77c4e
Angel Lee <angeljubilee@gmail.com>
2021-11-23 20:49:12 -0800

    Added leetcode

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -3,23 +3,34 @@
  * @return {string}
  */
 var removeDuplicates = function (s) {
-  let skippedChars = false;
-  let answer = '';
+  // let skippedChars = false;
+  // let answer = '';
+
+  // for (let i = 0; i < s.length; i++) {
+  //   if (s[i] === s[i + 1]) {
+  //     skippedChars = true;
+  //     i++;
+  //   } else {
+  //     answer += s[i];
+  //   }
+  // }
+
+  // console.log(answer);
+
+  // if (skippedChars) {
+  //   return removeDuplicates(answer);
+  // }
+  // return answer;
+
+  let stack = [];
 
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === s[i + 1]) {
-      skippedChars = true;
-      i++;
-    } else {
-      answer += s[i];
-    }
+    if (s[i] === stack[stack.length - 1])
+      stack.pop();
+    else
+      stack.push(s[i]);
+    console.log(stack)
   }
 
-  console.log(answer);
-
-  if (skippedChars) {
-    return removeDuplicates(answer);
-  }
-  return answer;
-
+  return stack.join('');
 };
